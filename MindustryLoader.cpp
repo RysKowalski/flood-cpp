d08f10bfe6ff545e20e75c9eb7bdf3fbe9d893d8
RysKowalski <ryskowalski0@gmail.com>
2026-06-11 09:37:56 +0200

    i

--- MindustryLoader.cpp
+++ MindustryLoader.cpp
@@ -1,4 +1,12 @@
 #include "MindustryLoader.hpp"
 #include "Grid.hpp"
 
+std::uint16_t Reader::read_u16() {
+  std::uint16_t value =
+      (static_cast<std::uint16_t>(data_[pos_]) << 8) | data_[pos_ + 1];
+
+  pos_ += 2;
+  return value;
+};
+
 Grid MindustryLoader::load_map(const std::string &path) { return Grid(5, 5); }
